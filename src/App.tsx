6e04be3a326f99ce7d1290a578856d2d090d0a04
s_feles_ <lukas.janicki@poczta.fm>
2025-05-10 15:06:18 +0200

    Added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Route, Routes } from "react-router";
 import Navbar from "./components/layout/Navbar"
 import MainPage from "./pages/Main/Main"
 
@@ -5,7 +6,9 @@ function App() {
   return (
     <div className="min-h-screen bg-gray-900 text-white">
       <Navbar />
-      <MainPage />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
     </div>
   )
 }
