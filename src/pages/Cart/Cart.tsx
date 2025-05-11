7fb888e292ff4a054bd77552e58365f4183fead3
s_feles_ <lukas.janicki@poczta.fm>
2025-05-11 15:59:07 +0200

    Implemented cart state as app-wide context

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -9,54 +9,6 @@ export interface CartProduct extends Product {
   quantity: number;
 }
 
-const initialCartItems: CartProduct[] = [
-  {
-    id: 1,
-    name: "Banany BIO",
-    price: {
-      main: 3,
-      fractional: 49,
-    },
-    quantity: 2,
-  },
-  {
-    id: 2,
-    name: "Mleko 3.2%",
-    price: {
-      main: 2,
-      fractional: 89,
-    },
-    quantity: 2
-  },
-  {
-    id: 3,
-    name: "Chleb Żytni",
-    price: {
-      main: 4,
-      fractional: 15,
-    },
-    quantity: 1
-  },
-  {
-    id: 4,
-    name: "Jaja 6 sztuk",
-    price: {
-      main: 6,
-      fractional: 99,
-    },
-    quantity: 1
-  },
-  {
-    id: 5,
-    name: "Łosoś wędzony",
-    price: {
-      main: 5,
-      fractional: 20,
-    },
-    quantity: 2
-  }
-];
-
 export default function Cart() {
   const { state } = useCart();
   return (
