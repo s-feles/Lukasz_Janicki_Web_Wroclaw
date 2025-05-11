4126453daa9da90dab1bb84732e99868f31adc0f
s_feles_ <lukas.janicki@poczta.fm>
2025-05-11 18:52:35 +0200

    Base change

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { resolve } from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: './',
+  base: '/Lukasz_Janicki_Web_Wroclaw/',
   build: {
     outDir: 'dist',
     rollupOptions: {
